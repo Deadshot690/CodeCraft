552ef5132df613f766e9e827da23a812ba528240
Juned Bhojani <junedbhojani61@gmail.com>
2025-09-04 09:25:33 +0000

    Try fixing this error: `Runtime Error: Error: Element type is invalid: e

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -2,7 +2,7 @@
 "use client"
 
 import * as React from "react"
-import { ImperativePanelHandle, PanelGroup, Panel } from "react-resizable-panels"
+import { ImperativePanelHandle, PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
@@ -31,10 +31,10 @@ ResizablePanel.displayName = "ResizablePanel"
 
 
 const ResizableHandle = React.forwardRef<
-  HTMLDivElement,
-  React.ComponentProps<"div"> & { withHandle?: boolean }
+  React.ElementRef<typeof PanelResizeHandle>,
+  React.ComponentProps<typeof PanelResizeHandle> & { withHandle?: boolean }
 >(({ className, withHandle, ...props }, ref) => (
-  <PanelGroup.Handle
+  <PanelResizeHandle
     ref={ref}
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-id]]:focus-visible:ring-offset-background",
@@ -45,7 +45,7 @@ const ResizableHandle = React.forwardRef<
     {withHandle && (
       <div className="z-10 flex h-4 w-1.5 items-center justify-center rounded-sm border bg-border" />
     )}
-  </PanelGroup.Handle>
+  </PanelResizeHandle>
 ))
 ResizableHandle.displayName = "ResizableHandle"
 
