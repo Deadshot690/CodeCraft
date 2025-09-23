d0b7d909bab66875526a80a74c7235299251572b
Juned Bhojani <junedbhojani61@gmail.com>
2025-09-23 15:25:26 +0000

    why the heck is my website not starting ?!

--- next.config.ts
+++ next.config.ts
@@ -30,9 +30,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // This is to allow the Next.js dev server to accept requests from the
-  // Firebase Studio environment.
-  allowedDevOrigins: ['*'],
+  experimental: {
+    // This is to allow the Next.js dev server to accept requests from the
+    // Firebase Studio environment.
+    allowedDevOrigins: ['*'],
+  }
 };
 
 export default nextConfig;
