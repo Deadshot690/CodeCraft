b4ceb5761f98a5fcc79c277f767315d441079acf
Juned Bhojani <junedbhojani61@gmail.com>
2025-09-29 08:25:11 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,13 +9,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "TODO: YOUR API KEY",
-  authDomain: "TODO: YOUR AUTH DOMAIN",
-  projectId: "TODO: YOUR PROJECT ID",
-  storageBucket: "TODO: YOUR STORAGE BUCKET",
-  messagingSenderId: "TODO: YOUR MESSAGING SENDER ID",
-  appId: "TODO: YOUR APP ID",
-  measurementId: "TODO: YOUR MEASUREMENT ID"
+  "projectId": "studio-2966886969-113dc",
+  "appId": "1:227757932645:web:a4aec5d5de91ca6692068c",
+  "apiKey": "AIzaSyBEDtKWSHo7lmraBTFvr325gz6qsdRbZVg",
+  "authDomain": "studio-2966886969-113dc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "227757932645"
 };
 
 // Initialize Firebase
